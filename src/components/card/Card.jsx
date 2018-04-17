ad65c143c0b7e420e1a96508d9ecf2caa0e1c24c
Ernie Yang <ernieyang09@hotmail.com>
2018-04-17 19:41:45 +0800

    delete useless code

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -10,10 +10,8 @@ class Card extends React.Component {
       <div className='card mb-4 box-shadow'>
         <img
           className='card-img-top'
-          data-src='holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail'
           alt='Thumbnail [100%x225]'
           src={img}
-          data-holder-rendered='true'
         />
         <div className='card-body'>
           <p className='card-text'>
