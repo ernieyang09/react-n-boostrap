7abd0ddc2debb2cf77f962122db0a4d7652ce452
Ernie Yang <ernieyang09@hotmail.com>
2018-04-17 17:20:28 +0800

    add webpack-cli & html-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: {
+    index: ['./src/index.jsx']
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        loader:'babel-loader',
+        test: /\.jsx?/,
+        include: path.resolve(__dirname, 'src')
+      },
+      // {
+      //    test: /\.css$/,
+      //    use: [ 'style-loader', 'css-loader' ]
+      // }
+    ]
+  },
+  devServer:{
+    port:3000,
+    publicPath: "/",
+    contentBase:'./src',
+    inline:true,
+  },
+  resolve: {
+    extensions: ['index.js', 'index.jsx', '.js', '.jsx'],
+    modules: ['node_modules'], // 指定套件的路徑
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ]
+}
